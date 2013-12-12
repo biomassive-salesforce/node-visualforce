ab31b54abdcca8833c66defa523d6b63e38f20e1
Amilcar J. Infante <amilcar.infante@globant.com>
2013-12-12 16:36:54 -0300

    62384252 - Create the static resource zip only if the static resource input folder has files

--- tasks/lib/staticResources.js
+++ tasks/lib/staticResources.js
@@ -3,6 +3,7 @@
 var grunt = require("grunt");
 var utils = require("./utils.js");
 var constants = require("./constants.js");
+var fs = require('fs');
 
 /**
  * library that helps to build package.
@@ -10,20 +11,51 @@ var constants = require("./constants.js");
  * @param {function} done  async flag
  * @return {void}
  */
-exports.buildStaticResources = function(options, done) {
-	
+exports.buildStaticResources = function(options, done) {	
 	//Get Static Resource Name from configuration object
 	var inputPath = options.inputPath;
 	var outputPath = options.outputPath;
 	var staticResourceName = options.staticResourceName;
 	var staticResourceFolder = options.staticResourceFolder;
 	
-	//Compress Static Resources
-	var zipper = require('./zipper.js')(grunt);
+	//Validates if the static resource folder has content to build the .resource and .resource-meta.xml files
+	var hasContent = false;
 
-	zipper.compress(inputPath + staticResourceFolder, outputPath + staticResourceFolder, staticResourceName, 
-					constants.STATIC_RESOURCE_EXTENSION, done);
+	//If the static resource folder exists
+	if ( fs.existsSync('./' + inputPath + staticResourceFolder )){
+		//Get the directories
+		var staticResources = fs.readdirSync('./' + inputPath + staticResourceFolder);
+		var files = [];
 
-	//Write Static Resource -meta.xml
-	utils.buildXMLMeta(constants.XML_META_STATIC_RESOURCE, staticResourceName, constants.STATIC_RESOURCE_EXTENSION, outputPath);
+		staticResources.some(function(staticResource){
+			var stat = fs.statSync('./' + inputPath + staticResourceFolder + staticResource + '/');
+			if(stat.isDirectory()){
+				files = fs.readdirSync('./' + inputPath + staticResourceFolder + staticResource + '/');
+				if(files && files.length > 0){
+					hasContent = true;
+				}
+			}else{
+				hasContent = true;
+			}
+
+			return hasContent;		
+		})
+		
+
+		if(hasContent){
+			//Compress Static Resources
+			var zipper = require('./zipper.js')(grunt);
+
+			zipper.compress(inputPath + staticResourceFolder, outputPath + staticResourceFolder, staticResourceName, 
+							constants.STATIC_RESOURCE_EXTENSION, done);
+
+			//Write Static Resource -meta.xml
+			utils.buildXMLMeta(constants.XML_META_STATIC_RESOURCE, staticResourceName, constants.STATIC_RESOURCE_EXTENSION, outputPath);
+		}else{
+			console.log(String('Your ' + inputPath + staticResourceFolder + ' folder is empty, the static resource build will be skipped').cyan);
+		}
+	}else{
+		console.log(String('Your ' + inputPath + staticResourceFolder + ' folder was not created, the static resource build will be skipped').cyan);
+	}
+	
 };
